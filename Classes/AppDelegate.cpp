f3ae8c4ad66255959007b97cf7ec595efbaa9d1c
Saumya Mukul <saumya.mukul@mx.com>
2016-11-02 12:51:16 -0600

    Add keyboard controls

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,63 +1,62 @@
-#include "Constants.h"
 #include "AppDelegate.h"
-#include "LevelScene.h""
-#include "Menu.h"
+#include "Constants.h"
 #include "GamePad.h"
+#include "LevelScene.h" "
+#include "Menu.h"
 
 USING_NS_CC;
 
-AppDelegate::AppDelegate() {
-}
+AppDelegate::AppDelegate() {}
 
-AppDelegate::~AppDelegate() {
-}
+AppDelegate::~AppDelegate() {}
 
 void AppDelegate::initGLContextAttrs() {
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
-    GLView::setGLContextAttrs(glContextAttrs);
+  GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+  GLView::setGLContextAttrs(glContextAttrs);
 }
 
-static int register_all_packages() {
-    return 0;
-}
+static int register_all_packages() { return 0; }
 
 bool AppDelegate::applicationDidFinishLaunching() {
-    // initialize director
-    auto director = Director::getInstance();
-    auto glview = director->getOpenGLView();
-    if(!glview) {
-        glview = GLViewImpl::createWithRect(GAME_NAME, cocos2d::Rect(0, 0, RESOLUTION_WIDTH, RESOLUTION_HEIGHT));
-        director->setOpenGLView(glview);
-    }
+  // initialize director
+  auto director = Director::getInstance();
+  auto glview = director->getOpenGLView();
+  if (!glview) {
+    glview = GLViewImpl::createWithRect(
+        GAME_NAME, cocos2d::Rect(0, 0, RESOLUTION_WIDTH, RESOLUTION_HEIGHT));
+    director->setOpenGLView(glview);
+  }
 
-    // turn on display FPS
-    director->setDisplayStats(true);
-    director->setAnimationInterval(FIXED_TIME_STEP);
-    glview->setDesignResolutionSize(RESOLUTION_WIDTH, RESOLUTION_HEIGHT, ResolutionPolicy::NO_BORDER);
-    director->setContentScaleFactor(0.5f);
+  // turn on display FPS
+  director->setDisplayStats(true);
+  director->setAnimationInterval(FIXED_TIME_STEP);
+  glview->setDesignResolutionSize(RESOLUTION_WIDTH, RESOLUTION_HEIGHT,
+                                  ResolutionPolicy::NO_BORDER);
+  director->setContentScaleFactor(0.5f);
 
-    register_all_packages();
+  register_all_packages();
 
-    auto scene = MenuScene::createScene();
-    director->runWithScene(scene);
-    
-    setupGamepad();
+  auto scene = LevelScene::createScene();
+  director->runWithScene(scene);
 
-    return true;
+  setupGamepad();
+
+  return true;
 }
 
-// This function will be called when the app is inactive. Note, when receiving a phone call it is invoked.
+// This function will be called when the app is inactive. Note, when receiving a
+// phone call it is invoked.
 void AppDelegate::applicationDidEnterBackground() {
-    Director::getInstance()->stopAnimation();
+  Director::getInstance()->stopAnimation();
 
-    // if you use SimpleAudioEngine, it must be paused
-    // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
+  // if you use SimpleAudioEngine, it must be paused
+  // SimpleAudioEngine::getInstance()->pauseBackgroundMusic();
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
-    Director::getInstance()->startAnimation();
+  Director::getInstance()->startAnimation();
 
-    // if you use SimpleAudioEngine, it must resume here
-    // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
+  // if you use SimpleAudioEngine, it must resume here
+  // SimpleAudioEngine::getInstance()->resumeBackgroundMusic();
 }
--- Classes/Character.cpp
+++ Classes/Character.cpp
@@ -4,177 +4,260 @@
 //
 //  Created by Josh Bodily on 10/18/16.
 //
-//til
+// til
 
-#include "Constants.h"
 #include "Character.h"
+#include "Constants.h"
 #include "GamePad.h"
 #include "Pickup.h"
 #include "ui/UILayout.h"
 
+#define KEYBOARD_ONLY 1
 const cocos2d::Vec2 GRAVITY(0.0f, GRAVITY_Y);
 
 bool Character::init() {
-    if (!Node::init()) return false;
-    
-    auto label = cocos2d::Label::create("P1", "fonts/ARCADECLASSIC.TTF", 48);
-    addChild(label);
-    label->enableOutline(cocos2d::Color4B::WHITE, 2);
-    label->setTextColor(cocos2d::Color4B::BLACK);
-    label->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE);
-    label->setPositionY(100);
-    
-    _sprite = cocos2d::Sprite::createWithSpriteFrameName("frame_0_delay-0.08s.gif");
-    _sprite->getTexture()->setAliasTexParameters();
-    addChild(_sprite);
-    
-    _bounds = cocos2d::Size(20.0f, 50.0f);
-#ifdef DEBUG_DRAW
-    _debugDraw = cocos2d::DrawNode::create();
-    _debugDraw->drawRect(cocos2d::Vec2(-_bounds.width * 0.5f, 0), cocos2d::Vec2(_bounds.width * 0.5f, _bounds.height), cocos2d::Color4F::BLUE);
-    addChild(_debugDraw);
-#endif
-    
-    // plot a parabola for throw path
-    _parabolaNode = Node::create();
-    addChild(_parabolaNode);
-    for (int i = 0; i < 20; ++i) {
-        auto sprite = cocos2d::Sprite::createWithSpriteFrameName("dot.png");
-        sprite->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE_BOTTOM);
-        sprite->getTexture()->setAliasTexParameters();
-        _parabolaNode->addChild(sprite);
-    }
-    
-    cocos2d::Vec2 position(500, 350);
-    setPosition(position);
-    _oldPosition = position;
-    _velocity = cocos2d::Vec2::ZERO;
+  if (!Node::init())
+    return false;
 
-    return true;
+#if KEYBOARD_ONLY
+  auto listener = cocos2d::EventListenerKeyboard::create();
+  listener->onKeyPressed = [this](cocos2d::EventKeyboard::KeyCode keyCode,
+                                  cocos2d::Event *event) {
+    cocos2d::Scheduler *scheduler =
+        cocos2d::Director::getInstance()->getScheduler();
+    if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_A) {
+      _velocity.x = 0.0f;
+      _previousControllerState.left = true;
+      _previousControllerState.right = false;
+    }
+    if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_D) {
+      _velocity.x = 0.0f;
+      _previousControllerState.right = true;
+      _previousControllerState.left = false;
+    }
+    if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_SPACE) {
+      _previousControllerState.a = true;
+    }
+  };
+  listener->onKeyReleased = [this](cocos2d::EventKeyboard::KeyCode keyCode,
+                                   cocos2d::Event *event) {
+    if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_A) {
+      _previousControllerState.left = false;
+    }
+    if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_D) {
+      _previousControllerState.right = false;
+    }
+    if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_SPACE) {
+      _previousControllerState.a = false;
+    }
+  };
+
+  cocos2d::Director::getInstance()
+      ->getEventDispatcher()
+      ->addEventListenerWithFixedPriority(listener, -1);
+#endif
+
+  auto label = cocos2d::Label::create("P1", "fonts/ARCADECLASSIC.TTF", 48);
+  addChild(label);
+  label->enableOutline(cocos2d::Color4B::WHITE, 2);
+  label->setTextColor(cocos2d::Color4B::BLACK);
+  label->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE);
+  label->setPositionY(100);
+
+  _sprite =
+      cocos2d::Sprite::createWithSpriteFrameName("frame_0_delay-0.08s.gif");
+  _sprite->getTexture()->setAliasTexParameters();
+  addChild(_sprite);
+
+  _bounds = cocos2d::Size(20.0f, 50.0f);
+#ifdef DEBUG_DRAW
+  _debugDraw = cocos2d::DrawNode::create();
+  _debugDraw->drawRect(cocos2d::Vec2(-_bounds.width * 0.5f, 0),
+                       cocos2d::Vec2(_bounds.width * 0.5f, _bounds.height),
+                       cocos2d::Color4F::BLUE);
+  addChild(_debugDraw);
+#endif
+
+  // plot a parabola for throw path
+  _parabolaNode = Node::create();
+  addChild(_parabolaNode);
+  for (int i = 0; i < 20; ++i) {
+    auto sprite = cocos2d::Sprite::createWithSpriteFrameName("dot.png");
+    sprite->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE_BOTTOM);
+    sprite->getTexture()->setAliasTexParameters();
+    _parabolaNode->addChild(sprite);
+  }
+
+  cocos2d::Vec2 position(500, 350);
+  setPosition(position);
+  _oldPosition = position;
+  _velocity = cocos2d::Vec2::ZERO;
+
+  return true;
 }
 
-void Character::resolveCollision(Collider* collider, const std::vector<cocos2d::Vec2>& collisionVectors) {
-    
-    auto position = getPosition();
-    auto closest = Util::findClosestResolution(collisionVectors, position, _oldPosition);
-    
-    // Collision with level
-    if (collider->getTag() == 0) {
-        if (closest.getNormalized().dot(cocos2d::Vec2::UNIT_Y) > 0.5f) {
-            _velocity.y = 0.0f;
-            _onGround = true;
-        }
-        if (closest.getNormalized().dot(cocos2d::Vec2::UNIT_Y) < 0.0f) {
-            _velocity.y = 0.0f;
-        }
-        if (closest.getNormalized().dot(cocos2d::Vec2::UNIT_X) > 0.5f) {
-            _velocity.x = 0.0f;
-        }
-        position += closest;
-        setPosition(position);
+void Character::resolveCollision(
+    Collider *collider, const std::vector<cocos2d::Vec2> &collisionVectors) {
+
+  auto position = getPosition();
+  auto closest =
+      Util::findClosestResolution(collisionVectors, position, _oldPosition);
+
+  // Collision with level
+  if (collider->getTag() == 0) {
+    if (closest.getNormalized().dot(cocos2d::Vec2::UNIT_Y) > 0.5f) {
+      _velocity.y = 0.0f;
+      _onGround = true;
     }
-    // Pickups
-    else if (collider->getTag() == WEAPON_TAG && !closest.equals(cocos2d::Vec2::ZERO)) {
-        if (Util::getControllerState(0).down && Util::getControllerState(0).x) {
-            collider->setFlags(0);
-            _heldItem = static_cast<PickupableInterface*>(collider->getParent());
-        }
+    if (closest.getNormalized().dot(cocos2d::Vec2::UNIT_Y) < 0.0f) {
+      _velocity.y = 0.0f;
     }
+    if (closest.getNormalized().dot(cocos2d::Vec2::UNIT_X) > 0.5f) {
+      _velocity.x = 0.0f;
+    }
+    position += closest;
+    setPosition(position);
+  }
+  // Pickups
+  else if (collider->getTag() == WEAPON_TAG &&
+           !closest.equals(cocos2d::Vec2::ZERO)) {
+    if (Util::getControllerState(0).down && Util::getControllerState(0).x) {
+      collider->setFlags(0);
+      _heldItem = static_cast<PickupableInterface *>(collider->getParent());
+    }
+  }
 }
 
 void Character::update(float dt) {
-    dt = FIXED_TIME_STEP;
-    
-    cocos2d::Vec2 forces = GRAVITY;
+  dt = FIXED_TIME_STEP;
 
-    if (std::fabs(_velocity.x) > 20.0f) {
-        _sprite->setFlippedX(_velocity.x < 0.0);
-        if (_sprite->getNumberOfRunningActions() == 0) {
-            // TODO: Create static animation map (name => animation) in init
-            auto animation = cocos2d::AnimationCache::getInstance()->getAnimation("run");
-            auto animate = cocos2d::Animate::create(animation);
-            _sprite->getSpriteFrame()->setAnchorPoint(cocos2d::Vec2::ANCHOR_MIDDLE_BOTTOM);
-            _sprite->runAction(animate);
-        }
-    } else {
-        _sprite->stopAllActions();
-        _sprite->setSpriteFrame("frame_1_delay-0.08s.gif");
+  cocos2d::Vec2 forces = GRAVITY;
+
+  if (std::fabs(_velocity.x) > 20.0f) {
+    _sprite->setFlippedX(_velocity.x < 0.0);
+    if (_sprite->getNumberOfRunningActions() == 0) {
+      // TODO: Create static animation map (name => animation) in init
+      auto animation =
+          cocos2d::AnimationCache::getInstance()->getAnimation("run");
+      auto animate = cocos2d::Animate::create(animation);
+      _sprite->getSpriteFrame()->setAnchorPoint(
+          cocos2d::Vec2::ANCHOR_MIDDLE_BOTTOM);
+      _sprite->runAction(animate);
     }
-    
-    // Jumping
-    if (Util::getControllerState(0).a && _onGround) {
-        _jumpForces = 7000.0f;
+  } else {
+    _sprite->stopAllActions();
+    _sprite->setSpriteFrame("frame_1_delay-0.08s.gif");
+  }
+#if KEYBOARD_ONLY
+  if (_previousControllerState.a && _onGround) {
+    _jumpForces = 7000.0f;
+  }
+  if (_previousControllerState.a)
+    _jumpForces *= 0.9f;
+  else
+    _jumpForces *= 0.5f;
+
+#else
+  // Jumping
+  if (Util::getControllerState(0).a && _onGround) {
+    _jumpForces = 7000.0f;
+  }
+  if (Util::getControllerState(0).a)
+    _jumpForces *= 0.9f;
+  else
+    _jumpForces *= 0.5f;
+#endif
+
+  // Accumulate forces and update
+  forces += cocos2d::Vec2(0.0f, _jumpForces);
+  _oldPosition = getPosition();
+  setPosition(getPosition() + _velocity * dt);
+  _velocity += (forces / 1.0f) * dt;
+
+// Left / Right Movement
+#if KEYBOARD_ONLY
+  if (_previousControllerState.left) {
+    CCLOG("LEFT");
+    _velocity.x -= 15.0f;
+    if (_velocity.x < -250.0f)
+      _velocity.x = -250.0f;
+  }
+
+  if (_previousControllerState.right) {
+    CCLOG("RIGHT");
+    _velocity.x += 15.0f;
+    if (_velocity.x > 250.0f)
+      _velocity.x = 250.0f;
+  }
+#else
+  if (Util::getControllerState(0).left) {
+    if (!_previousControllerState.left)
+      _velocity.x = 0.0f;
+    _velocity.x -= 15.0f;
+    if (_velocity.x < -250.0f)
+      _velocity.x = -250.0f;
+  }
+  if (Util::getControllerState(0).right) {
+    if (!_previousControllerState.right)
+      _velocity.x = 0.0f;
+    _velocity.x += 15.0f;
+    if (_velocity.x > 250.0f)
+      _velocity.x = 250.0f;
+  }
+#endif
+  // Ground Friction
+  if (!Util::getControllerState(0).left && !Util::getControllerState(0).right &&
+      _onGround) {
+    _velocity.x *= 0.5f;
+  }
+
+  // Draw parabolic arc for aiming thrown items
+  if (Util::getControllerState(0).right_stick.length() > 8000 &&
+      _heldItem != nullptr && Util::isThrowableItem(_heldItem)) {
+
+    cocos2d::Vec2 points[20];
+    float t = 0.1f;
+    for (int i = 0; i < 20; ++i) {
+      points[i].x = 0.0f + Util::getControllerState(0).right_stick.x * t * 0.02;
+      points[i].y = 32.0f +
+                    Util::getControllerState(0).right_stick.y * t * -0.03 +
+                    -1000 * t * t;
+      t += 0.1f;
     }
-    if (Util::getControllerState(0).a)
-        _jumpForces *= 0.9f;
-    else
-        _jumpForces *= 0.5f;
-    
-    // Accumulate forces and update
-    forces += cocos2d::Vec2(0.0f, _jumpForces);
-    _oldPosition = getPosition();
-    setPosition(getPosition() + _velocity * dt);
-    _velocity += ( forces / 1.0f ) * dt;
-    
-    // Left / Right Movement
-    if (Util::getControllerState(0).left) {
-        if (!_previousControllerState.left)
-            _velocity.x = 0.0f;
-        _velocity.x -= 15.0f;
-        if (_velocity.x < -250.0f)
-            _velocity.x = -250.0f;
+    int i = 0;
+    for (auto &child : _parabolaNode->getChildren()) {
+      if (points[i].x > 800 || points[i].y > 800)
+        child->setVisible(false);
+      else
+        child->setVisible(true);
+
+      child->setPosition(points[i++]);
     }
-    if (Util::getControllerState(0).right) {
-        if (!_previousControllerState.right)
-            _velocity.x = 0.0f;
-        _velocity.x += 15.0f;
-        if (_velocity.x > 250.0f)
-            _velocity.x = 250.0f;
-    }
-    
-    // Ground Friction
-    if (!Util::getControllerState(0).left && !Util::getControllerState(0).right && _onGround) {
-        _velocity.x *= 0.5f;
-    }
-    
-    // Draw parabolic arc for aiming thrown items
-    if (Util::getControllerState(0).right_stick.length() > 8000 && _heldItem != nullptr && Util::isThrowableItem(_heldItem)) {
-        
-        cocos2d::Vec2 points[20];
-        float t = 0.1f;
-        for (int i = 0; i < 20; ++i) {
-            points[i].x = 0.0f + Util::getControllerState(0).right_stick.x * t * 0.02;
-            points[i].y = 32.0f + Util::getControllerState(0).right_stick.y * t * -0.03 + -1000 * t * t;
-            t += 0.1f;
-        }
-        int i = 0;
-        for (auto &child : _parabolaNode->getChildren()) {
-            if (points[i].x > 800 || points[i].y > 800) child->setVisible(false);
-            else child->setVisible(true);
-            
-            child->setPosition(points[i++]);
-        }
-        _parabolaNode->setVisible(true);
-        
-    } else {
-        _parabolaNode->setVisible(false);
-    }
-    
-    // Pick-ups, display as carried
-    if (_heldItem != nullptr) {
-        _heldItem->setPosition(getPosition());
-    }
-    
-    // Throw item
-    if (Util::getControllerState(0).right_stick.length() > 8000 && Util::getControllerState(0).x && !_previousControllerState.x && _heldItem != nullptr) {
-        float x = Util::getControllerState(0).right_stick.x * 0.02;
-        float y = Util::getControllerState(0).right_stick.y * 0.02;
-        _heldItem->setVelocity(cocos2d::Vec2(x, y));
-        _heldItem->getCollideable()->setFlags(COLLIDE_ALL_SIDES);
-        _heldItem = nullptr;
-    }
-    
-    // Finalize update
-    _previousControllerState = Util::getControllerState(0);
-    _onGround = false;
+    _parabolaNode->setVisible(true);
+
+  } else {
+    _parabolaNode->setVisible(false);
+  }
+
+  // Pick-ups, display as carried
+  if (_heldItem != nullptr) {
+    _heldItem->setPosition(getPosition());
+  }
+
+  // Throw item
+  if (Util::getControllerState(0).right_stick.length() > 8000 &&
+      Util::getControllerState(0).x && !_previousControllerState.x &&
+      _heldItem != nullptr) {
+    float x = Util::getControllerState(0).right_stick.x * 0.02;
+    float y = Util::getControllerState(0).right_stick.y * 0.02;
+    _heldItem->setVelocity(cocos2d::Vec2(x, y));
+    _heldItem->getCollideable()->setFlags(COLLIDE_ALL_SIDES);
+    _heldItem = nullptr;
+  }
+
+// Finalize update
+#ifndef KEYBOARD_ONLY
+  _previousControllerState = Util::getControllerState(0);
+#endif
+  _onGround = false;
 }
